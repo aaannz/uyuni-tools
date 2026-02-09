2b9d48ab57658e1417e7816b5cab7d69b932bb31
Ondrej Holecek <oholecek@suse.com>
2026-02-09 16:59:04 +0100

    Allow setting up database backup during other ops
    Install, upgrade and migrate has --db-walbackup option now

--- mgradm/cmd/install/podman/utils.go
+++ mgradm/cmd/install/podman/utils.go
@@ -12,6 +12,7 @@ import (
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
+	"github.com/uyuni-project/uyuni-tools/mgradm/cmd/backup/db"
 	"github.com/uyuni-project/uyuni-tools/mgradm/shared/coco"
 	"github.com/uyuni-project/uyuni-tools/mgradm/shared/hub"
 	"github.com/uyuni-project/uyuni-tools/mgradm/shared/pgsql"
@@ -106,6 +107,11 @@ func installForPodman(
 		// Run the DB container setup if the user doesn't set a custom host name for it.
 		if err := pgsql.SetupPgsql(systemd, preparedPgsqlImage); err != nil {
 			return err
+		}
+		if flags.Installation.DB.Walbackup {
+			if err := db.Enable(true); err != nil {
+				return err
+			}
 		}
 	} else {
 		log.Info().Msgf(
